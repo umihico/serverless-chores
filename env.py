3a60f0d9483db618f5bcf4988aa5447c85bd4a2e
umihico <umihico@users.noreply.github.com>
2020-02-22 13:16:11 +0900

    specify boto3 profile

--- env.py
+++ env.py
@@ -3,10 +3,12 @@ import boto3
 import yaml
 
 if os.getenv("STAGE", "local") == "local":
+    session = boto3.Session(profile_name='umihico')
+    ssm = session.client('ssm')
     with open('./env/local.yml') as file:
-        for key, value in yaml.load(file).items():
-            os.environ[key] = value if not value.startswith("${ssm:") else boto3.client(
-                'ssm').get_parameter(Name=value[6:-1])['Parameter']['Value']
+        for key, value in yaml.safe_load(file).items():
+            os.environ[key] = value if not value.startswith(
+                "${ssm:") else ssm.get_parameter(Name=value[6:-1])['Parameter']['Value']
 
 
 def test_get():
