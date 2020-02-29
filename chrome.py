1e72748f197975d47dc8a033506f1173455429cf
umihico <umihico@users.noreply.github.com>
2020-02-29 16:40:34 +0900

    add chrome

--- chrome.py
+++ chrome.py
@@ -5,57 +5,38 @@ import time
 import env
 
 
-def Options():
+def get_options():
     options = webdriver.ChromeOptions()
-    if os.environ['STAGE'] != "local":
-        options.binary_location = "/opt/python/bin/headless-chromium"
-        options.add_argument("--headless")
-        options.add_argument("--homedir=/tmp")
-        options.add_argument("--disable-gpu")
-        options.add_argument("--window-size=1280x1696")
-        options.add_argument("--disable-application-cache")
-        options.add_argument("--disable-infobars")
-        options.add_argument("--no-sandbox")
-        options.add_argument("--hide-scrollbars")
-        options.add_argument("--enable-logging")
-        options.add_argument("--log-level=0")
-        options.add_argument("--single-process")
-        options.add_argument("--ignore-certificate-errors")
+    options.binary_location = "/opt/python/bin/headless-chromium"
+    options.add_argument("--headless")
+    options.add_argument("--homedir=/tmp")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--window-size=1280x1696")
+    options.add_argument("--disable-application-cache")
+    options.add_argument("--disable-infobars")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--hide-scrollbars")
+    options.add_argument("--enable-logging")
+    options.add_argument("--log-level=0")
+    options.add_argument("--single-process")
+    options.add_argument("--ignore-certificate-errors")
     return options
 
 
-def Chrome():
-    chrome = webdriver.Chrome(
-        os.getenv("CHROME_EXECUTABLE_PATH"), options=Options())
-    return chrome
-
-
-webdriver.Chrome.xpaths = webdriver.Chrome.find_elements_by_xpath
-webdriver.Chrome.xpath = webdriver.Chrome.find_element_by_xpath
-
-
-def wait_element(self, xpath, timeout=None):
-    start_time = time.time()
-    for _ in itertools.count():
-        if len(self.xpaths(xpath)):
-            break
-        if timeout and time.time() > start_time + timeout:
-            raise Exception(
-                ' '.join(["timeoutException. xpath:", xpath,  "is not found"]))
-        time.sleep(0.1)
-
-
-webdriver.Chrome.wait_element = wait_element
+class ServerlessChrome(webdriver.Chrome):
+    def __init__(self):
+        kw = {'executable_path': "/opt/python/bin/chromedriver", 'options': get_options(
+        )} if os.environ['STAGE'] != "local" else {}
+        super().__init__(**kw)
 
 
 def test_chrome():
-    chrome = Chrome()
+    chrome = ServerlessChrome()
     try:
         chrome.get("https://www.google.com/")
-        assert len(chrome.xpaths("//input")) > 0
+        chrome.find_element_by_xpath("//input")
         title = chrome.title
         assert title == "Google"
-        chrome.wait_element("//img")
     except Exception:
         raise
     finally:
